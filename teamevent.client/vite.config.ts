182e7684d200ba1fec48fc4ed3481ff9e26e2397
danijel88 <danijelboksan@gmail.com>
2025-04-07 18:05:17 +0200

    Update vite.config.ts

--- teamevent.client/vite.config.ts
+++ teamevent.client/vite.config.ts
@@ -1,11 +1,12 @@
 import { fileURLToPath, URL } from 'node:url';
-
 import { defineConfig } from 'vite';
 import plugin from '@vitejs/plugin-react';
 import fs from 'fs';
 import path from 'path';
 import child_process from 'child_process';
 import { env } from 'process';
+
+const isCI = !!env.CI || !!env.VERCEL;
 
 const baseFolder =
     env.APPDATA !== undefined && env.APPDATA !== ''
@@ -16,28 +17,34 @@ const certificateName = "teamevent.client";
 const certFilePath = path.join(baseFolder, `${certificateName}.pem`);
 const keyFilePath = path.join(baseFolder, `${certificateName}.key`);
 
-if (!fs.existsSync(baseFolder)) {
-    fs.mkdirSync(baseFolder, { recursive: true });
-}
+if (!isCI) {
+    if (!fs.existsSync(baseFolder)) {
+        fs.mkdirSync(baseFolder, { recursive: true });
+    }
 
-if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
-    if (0 !== child_process.spawnSync('dotnet', [
-        'dev-certs',
-        'https',
-        '--export-path',
-        certFilePath,
-        '--format',
-        'Pem',
-        '--no-password',
-    ], { stdio: 'inherit', }).status) {
-        throw new Error("Could not create certificate.");
+    if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
+        if (0 !== child_process.spawnSync('dotnet', [
+            'dev-certs',
+            'https',
+            '--export-path',
+            certFilePath,
+            '--format',
+            'Pem',
+            '--no-password',
+        ], { stdio: 'inherit', }).status) {
+            throw new Error("Could not create certificate.");
+        }
     }
 }
 
-const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
-    env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:7019';
+const target = env.ASPNETCORE_HTTPS_PORT
+    ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}`
+    : env.ASPNETCORE_URLS
+        ? env.ASPNETCORE_URLS.split(';')[0]
+        : 'https://localhost:7019';
+
 const tenantId = "tenant1";
-// https://vitejs.dev/config/
+
 export default defineConfig({
     plugins: [plugin()],
     resolve: {
@@ -52,7 +59,7 @@ export default defineConfig({
                 secure: false,
                 changeOrigin: true,
                 headers: {
-                    'X-Tenant-ID': tenantId, // Directly add custom header here
+                    'X-Tenant-ID': tenantId,
                 },
             },
             '^/TeamEvent/AddEvent': {
@@ -60,14 +67,16 @@ export default defineConfig({
                 secure: false,
                 changeOrigin: true,
                 headers: {
-                    'X-Tenant-ID': tenantId, // Directly add custom header here
+                    'X-Tenant-ID': tenantId,
                 },
             },
         },
         port: parseInt(env.DEV_SERVER_PORT || '63698'),
-        https: {
-            key: fs.readFileSync(keyFilePath),
-            cert: fs.readFileSync(certFilePath),
-        }
+        ...(isCI ? {} : {
+            https: {
+                key: fs.readFileSync(keyFilePath),
+                cert: fs.readFileSync(certFilePath),
+            }
+        })
     }
-})
+});
